1de921f7c916ca2dd6f5037e5351f57b1511af6e
Gabor <gabordesigns@gmail.com>
2022-12-03 00:29:42 +0100

    Update lsp keymap

--- lua/keymap/lsp.lua
+++ lua/keymap/lsp.lua
@@ -34,5 +34,5 @@ vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 vim.keymap.set('n', '<space>f', function()
-   vim.lsp.buf.format { async = true} 
+   vim.lsp.buf.format { async = true }
 end, bufopts)
