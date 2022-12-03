07a41e707761a2d37647452bf72a2072a2aa5ff1
GaborDesigns <gabordesigns@gmail.com>
2022-12-03 11:48:24 +0100

    Update lsp config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,55 +1,36 @@
 require('mason').setup()
-require('mason-lspconfig').setup()
-
-local lsp_flags = {
-    debounce_text_changes = 150, -- Default in nvim 0.7+
-}
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-require("mason-lspconfig").setup_handlers {
+require('mason-lspconfig').setup({
     -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "sumneko_lua" }
     -- This setting has no relation with the `automatic_installation` setting.
     ensure_installed = {
         "intelephense",
         "tailwindcss",
-        "volar", -- VueJS
-        "sumneko_lua", -- Lua
-        "dockerls",
-        "astro",
+        "sumneko_lua",
     },
+})
 
-    -- The first entry ( without a key ) will be the default handler
-    -- and will be called for each installed server that doens't have a dedicated handler
-    function (server_name)
-        require("lspconfig")[server_name].setup {
-            flags = lsp_flags,
-            capabilities = capabilities,
-        }
-    end,
+local lsp_flags = {
+    debounce_text_changes = 150, -- Default in nvim 0.7+
 }
 
 -- luasnip setup
 local luasnip = require 'luasnip'
 
+
 -- nvim-cmp setup
 local cmp = require 'cmp'
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
 
+cmp.setup({
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
@@ -69,26 +50,54 @@ cmp.setup({
             end
         end, { 'i', 's' }),
     }),
-
-    source = {
+    sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'nvim_lsp_signature_help' },
-    },
+        { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'buffer' },
+    })
 })
 
-local on_attach = function(_, bufnr)
-    return require('completion').on_attach
-end
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+        { name = 'buffer' },
+    })
+})
 
-require('lspconfig')['intelephense'].setup({
-    on_attach = on_attach,
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig')['intelephense'].setup {
     capabilities = capabilities,
-})
+}
 
-require('lspconfig')['tailwindcss'].setup({
+require('lspconfig')['tailwindcss'].setup {
     capabilities = capabilities,
-})
+}
 
 require('lspconfig')['sumneko_lua'].setup {
     settings = {
