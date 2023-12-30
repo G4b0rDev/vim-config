0323efb748a68b5f5cdb578b01c128826c4768c9
GaborDesigns <gabordesigns@gmail.com>
2023-12-30 11:29:56 +0100

    Upgrade telescope version

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,7 +6,7 @@ return require('packer').startup(function(use)
     -- Telescope
     use {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.0',
+        tag = '0.1.x',
         requires = {
             { 'nvim-lua/plenary.nvim' }
         }
