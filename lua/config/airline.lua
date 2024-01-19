39f18d5155ef8b7ee8b0bfcea3e2aa06392a6264
Gabor <gabordesigns@gmail.com>
2024-01-19 11:55:38 +0100

    Update airline theme to catppuccin

--- lua/config/airline.lua
+++ lua/config/airline.lua
@@ -2,5 +2,5 @@
 -- See: https://github.com/vim-airline/vim-airline
 
 vim.g.airline_extensions = { 'branch' }
-vim.g.airline_theme = 'base16_adwaita'
+vim.g.airline_theme = 'catppuccin'
 vim.g.airline_powerline_fonts = 1
