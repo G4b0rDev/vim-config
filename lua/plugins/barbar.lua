1fa888c6221efc2c11e8cd7bab0c665990c9563d
gaborm98 <gabordesigns@gmail.com>
2024-10-02 14:10:27 +0200

    chore: fix navigating buffer with barbar

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -3,12 +3,12 @@
 return {
     'romgrk/barbar.nvim',
     requires = {
-        {'nvim-tree/nvim-web-devicons'}
+        { 'nvim-tree/nvim-web-devicons' }
     },
     event = 'BufWinEnter',
-    config = function ()
+    config = function()
         -- config
-        require'bufferline'.setup {
+        require 'bufferline'.setup {
             -- Enable/Disable animations
             animation = false,
 
@@ -86,14 +86,14 @@ return {
         map('n', '<A-l>', '<Cmd>BufferNext<CR>', opts)
 
         -- Goto buffer in position
-        map('n', '<A-1>', '<Cmd>BufferGoTo 1<CR>', opts)
-        map('n', '<A-2>', '<Cmd>BufferGoTo 2<CR>', opts)
-        map('n', '<A-3>', '<Cmd>BufferGoTo 3<CR>', opts)
-        map('n', '<A-4>', '<Cmd>BufferGoTo 4<CR>', opts)
-        map('n', '<A-5>', '<Cmd>BufferGoTo 5<CR>', opts)
-        map('n', '<A-6>', '<Cmd>BufferGoTo 6<CR>', opts)
-        map('n', '<A-7>', '<Cmd>BufferGoTo 7<CR>', opts)
-        map('n', '<A-8>', '<Cmd>BufferGoTo 8<CR>', opts)
+        map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+        map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+        map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+        map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+        map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+        map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+        map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+        map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
         map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
 
         -- Pin/unpin buffer
