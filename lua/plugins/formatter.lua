acb36a3f13009c0a79f1c66ce6ba7ff15cbfcd05
G4b0rDev <gabordesigns@gmail.com>
2025-08-03 13:33:37 +0200

    chore: formatter config

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,48 +3,24 @@
 return {
     'stevearc/conform.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-        -- config
-        local conform = require('conform')
-
-        conform.setup({
-            formattery_by_ft = {
-                javascript = { 'prettier' },
-                typescript = { 'prettier' },
-                svelte = { 'prettier' },
-                css = { 'prettier' },
-                html = { 'prettier' },
-                json = { 'prettier' },
-                markdown = { 'prettier' },
-                lua = { 'stylua' },
-                php = { 'pint' },
-                blade = { 'bladeformatter' },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            },
-        })
-
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*.svelte",
-            callback = function()
-                conform.format({
-                    lsp_fallback = true,
-                    async = true,
-                    timeout_ms = 500,
-                })
-            end,
-        })
-
-        -- keymaps
-        vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
-            conform.format({
-                lsp_fallback = true,
-                async = true,
-                timeout_ms = 500,
-            })
-        end, { desc = 'Format file or range in visual mode' })
-    end
+    opts = {
+        formatters_by_ft = {
+            javascript = { 'prettier' },
+            typescript = { 'prettier' },
+            svelte = { 'prettier' },
+            css = { 'prettier' },
+            html = { 'prettier' },
+            json = { 'prettier' },
+            markdown = { 'prettier' },
+            lua = { 'stylua' },
+            php = { 'pint' },
+            blade = { 'bladeformatter' },
+        },
+        -- Set default options
+        default_format_opts = {
+            lsp_format = "fallback",
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 500 },
+    },
 }
