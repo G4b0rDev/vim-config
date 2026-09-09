964515477e3b612630aab15073c9d869882e0b06
G4b0rDev <gabordesigns@gmail.com>
2026-09-09 21:06:25 +0200

    feat: add laravel language server config

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -283,5 +283,19 @@ return {
         client.server_capabilities.documentRangeFormattingProvider = false
       end,
     })
+
+    vim.lsp.config("laravel_lsp", {
+      cmd = { "laravel-lsp" },
+      filetypes = { "php", "blade" },
+      root_dir = function(bufnr, on_dir)
+        local root = vim.fs.root(bufnr, "artisan")
+
+        if root then
+          on_dir(root)
+        end
+      end,
+    })
+
+    vim.lsp.enable("laravel_lsp")
   end,
 }
